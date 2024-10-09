8aa84b9c7e136304e920c3c67faecc6ebec03b1a
rinatyuk-git <17_ufo_RB@proton.me>
2024-10-09 15:06:56 -0400

    add Habit urls

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('habits.urls', namespace='habits')),
+    path('', include('habits.urls', namespace='habits')),
     path('', include('users.urls', namespace='users')),
 ]
