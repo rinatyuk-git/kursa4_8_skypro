2c63f966281711b39e8eee732e08d958595845c2
rinatyuk-git <17_ufo_RB@proton.me>
2024-11-01 12:41:59 +0100

    slight updates

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,11 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
+        # "NAME": os.getenv("NAME"),
         # "USER": os.getenv("DB_USER"),
+        # 'PASSWORD': os.getenv('PASSWORD'),
+
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
